84d56c06e1c3345aaf6014724fc94025140f6ec5
HiromuraDev <123483590+HiromuraDev@users.noreply.github.com>
2024-06-29 02:15:55 -0300

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: '/src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', 
+          'css-loader',   
+          'sass-loader',  
+        ],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, ''),
+    },
+    compress: true,
+    port: 8080,
+    hot: true,
+  },
+};
