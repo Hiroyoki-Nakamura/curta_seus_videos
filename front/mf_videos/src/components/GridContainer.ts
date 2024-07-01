92147eebcdf0eb387e7eaa2ca57e40da416c4d12
HiromuraDev <123483590+HiromuraDev@users.noreply.github.com>
2024-07-01 07:47:08 -0300

    feat:Add favorite icon

--- front/mf_videos/src/components/GridContainer.ts
+++ front/mf_videos/src/components/GridContainer.ts
@@ -2,11 +2,15 @@ import { VideoContainer } from "./VideoContainer";
 
 export class GridContainer {
     private grids: HTMLDivElement[];
+    private currentPage: number;
+    private videosPerPage: number;
     private allVideos: {title: string, url: string}[] = [];
     private displayedVideos: {title: string, url: string}[] = []
 
     constructor(private containerId: string, private numGrids: number){
         this.grids = [];
+        this.currentPage = 1;
+        this.videosPerPage = numGrids *3;
         this.initGrids();
     }
 
@@ -31,17 +35,35 @@ export class GridContainer {
         this.displayedVideos = this.allVideos.filter(video =>
             video.title.toLowerCase().includes(query.toLowerCase())
         );
+        this.currentPage = 1;
         this.renderVideos();
     }
 
     public renderVideos() {
-        
+        const start = (this.currentPage -1) * this.videosPerPage;
+        const end = start + this.videosPerPage;
+        const videosToRender = this.displayedVideos.slice(start, end);
+
         this.grids.forEach(grid => grid.innerHTML = '');
 
-        this.displayedVideos.forEach((videoData, index) => {
+        videosToRender.forEach((videoData, index) => {
             const grid = this.grids[index % this.numGrids];
             const videoComponent = new VideoContainer(videoData);
             grid.appendChild(videoComponent.getVideo())
         })
+    }
+
+    public nextPage() {
+        if((this.currentPage * this.videosPerPage) < this.displayedVideos.length){
+            this.currentPage++;
+            this.renderVideos();
+        }
+    }
+
+    public prevPage() {
+        if(this.currentPage > 1) {
+            this.currentPage--;
+            this.renderVideos();
+        }
     }
 }
--- front/mf_videos/src/index.ts
+++ front/mf_videos/src/index.ts
@@ -1,13 +1,53 @@
 import './styles/main.scss'
 import { GridContainer } from './components/GridContainer';
 
-const gridContainer = new GridContainer('container', 4)
+const videos = [
+    {title: 'Video 1', url: 'url1'},
+    {title: 'Video 2', url: 'url2'},
+    {title: 'Video 3', url: 'url3'},
+    {title: 'Video 4', url: 'url4'},
+    {title: 'Video 5', url: 'url5'},
+    {title: 'Video 6', url: 'url6'},
+    {title: 'Video 7', url: 'url7'},
+    {title: 'Video 8', url: 'url8'},
+    {title: 'Video 9', url: 'url9'},
+    {title: 'Video 10', url: 'url10'},
+    {title: 'Video 11', url: 'url11'},
+    {title: 'Video 12', url: 'url12'},
+    {title: 'Video 13', url: 'url13'},
+]
+
+const gridContainer = new GridContainer('container', 4);
+gridContainer.setVideos(videos);
 
 document.getElementById('searchButton')?.addEventListener('click', () => {
     const query = (document.getElementById('searchInput') as HTMLInputElement).value;
     gridContainer.searchVideos(query);
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+    const favorite = document.getElementById('favorite') as HTMLElement;
+    console.log('ola');
 
+    if (favorite) {
+        console.log('Elemento favoriteStar encontrado');
 
+        favorite.addEventListener('click', () => {
+            console.log('Elemento favorito clicado');
+            if (favorite.classList.contains('favorite-active')) {
+                console.log('Removendo a classe active');
+                favorite.classList.remove('favorite-active');
+                favorite.classList.add('favorite');
+            } else {
+                console.log('Adicionando a classe active');
+                favorite.classList.remove('favorite');
+                favorite.classList.add('favorite-active');
+            }
+        });
+    } else {
+        console.log('Elemento favoriteStar não encontrado');
+    }
+});
 
+document.getElementById('nextPage')?.addEventListener('click', () => gridContainer.nextPage());
+document.getElementById('prevPage')?.addEventListener('click', () => gridContainer.prevPage());
