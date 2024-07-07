097a63de1720443d811bcf22e868d1f91356635f
HiromuraDev <123483590+HiromuraDev@users.noreply.github.com>
2024-07-07 06:34:55 -0300

    refactor: Code indentation

--- front/mf_videos/src/index.ts
+++ front/mf_videos/src/index.ts
@@ -1,19 +1,25 @@
-import './styles/main.scss';
-import { GridContainer } from './components/GridContainer';
+import "./styles/main.scss";
+import { GridContainer } from "./components/GridContainer";
 
-document.addEventListener('DOMContentLoaded', () => {
-    const gridContainer = new GridContainer('container', 4);
-    const searchInput = document.getElementById('search-input') as HTMLInputElement;
+document.addEventListener("DOMContentLoaded", () => {
+  const gridContainer = new GridContainer("container", 4);
+  const searchInput = document.getElementById(
+    "search-input"
+  ) as HTMLInputElement;
 
-    gridContainer.setVideos([]); 
+  gridContainer.setVideos([]);
 
-    searchInput.addEventListener('keypress', function(event) {
-        if (event.key === 'Enter') {
-            const query = searchInput.value.trim();
-            gridContainer.searchVideos(query); 
-        }
-    });
+  searchInput.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      const query = searchInput.value.trim();
+      gridContainer.searchVideos(query);
+    }
+  });
 
-    document.getElementById('nextPage')?.addEventListener('click', () => gridContainer.nextPage());
-    document.getElementById('prevPage')?.addEventListener('click', () => gridContainer.prevPage());
+  document
+    .getElementById("nextPage")
+    ?.addEventListener("click", () => gridContainer.nextPage());
+  document
+    .getElementById("prevPage")
+    ?.addEventListener("click", () => gridContainer.prevPage());
 });
